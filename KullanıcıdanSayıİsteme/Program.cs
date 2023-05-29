04cfa5c6637f21175176fc8b5a1318b458f8f819
Aysun YILDIRIM <82328345+aysunyildirim@users.noreply.github.com>
2023-05-29 22:00:02 +0300

    Add project files.

---
+++ KullanıcıdanSayıİsteme/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine(" 1 ile 5 arasında bir değer giriniz");
+
+int girilen = Convert.ToInt32(Console.ReadLine());
+double can = Math.PI;  //PI sayısı 3 e tanimlanmıs.
+int kusursuz = Convert.ToInt32(can);
+Console.WriteLine(kusursuz*girilen);
+Console.ReadLine();
